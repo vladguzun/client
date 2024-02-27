12db5e2f151ccb2c696e7dae83ace2f459e21987
vladguzun <vladguzun20022@gmail.com>
2024-02-27 15:13:30 -0800

    aegesgsegesh

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const NotificationListener = () => {
   const [notification, setNotification] = useState<string>("");
 
   useEffect(() => {
-    const newSocket = io("https://server-2-no7z.onrender.com/");
+    const newSocket = io("https://server-3-gl8x.onrender.com/");
     setSocket(newSocket);
 
     newSocket.on('connect', () => {
